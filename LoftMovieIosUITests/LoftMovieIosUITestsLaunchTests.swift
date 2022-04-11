f1732a312f4403321724e7151babcf7685a0be90
ruslansamarbaev <samarbaev36@gmail.com>
2022-04-11 11:33:32 +0300

    Initial Commit

---
+++ LoftMovieIosUITests/LoftMovieIosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LoftMovieIosUITestsLaunchTests.swift
+//  LoftMovieIosUITests
+//
+//  Created by Руслан Самарбаев on 11.04.2022.
+//
+
+import XCTest
+
+class LoftMovieIosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
